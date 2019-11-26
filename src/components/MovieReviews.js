4cfec72ebb6630ec03b58af81c17f63fa0d8b0d0
swdecker <swdecker@gmail.com>
2019-11-26 16:45:29 -0600

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,15 @@
-// Code MovieReviews Here
+import React from 'react'
+import Movie from './Movie'
+const MoviewReviews = (props)=>{
+    const renderMovies=()=>(
+        props.reviews.map(review=><Movie className="movie" movie={review}/>)
+    )
+    return (
+        <div className={'review-list'}>
+            movielist
+            {props.reviews.map(review=><Movie className="movie" movie={review}/>)}
+        </div>
+    )
+}
+
+export default MoviewReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,45 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
+
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+
+export default class SearchableMovieReviewsContainer extends Component {
+    state={
+        searchTerm:"",
+        reviews:[]
+    }
+    handleTextChange=(event)=>{
+        this.setState({
+            searchTerm:event.target.value
+        })
+    }
+    handleSearchSubmit=(event)=>{
+        event.preventDefault()
+        fetch(URL+`&query=${this.state.searchTerm}`)
+        .then(resp=>resp.json())
+        .then(response=>{
+            this.setState({
+                reviews:response.results
+            })
+        })
+    }
+
+
+    render() {
+        return (
+            <div className={'searchable-movie-reviews'}>
+                <form onSubmit={this.handleSearchSubmit}>
+                    <input type={"text"} value={this.state.searchTerm} onChange={this.handleTextChange}/>
+                    <input type={"submit"} />
+                </form>
+                {this.state.reviews.length>0 && <MovieReviews reviews={this.state.reviews}/>}
+            </div>
+        )
+    }
+}
